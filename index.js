1465601c020dd29bbb3d9794faceb5ebec0e7b01
siddique <siddiquemd161@gmail.com>
2025-08-28 07:37:56 +0600

    history section done

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // index.js
 let heartNav=document.getElementById("nav-heart")
 let heart=document.getElementById("card-heart")
-let callButton=document.getElementById("call-btn")
-let serviceName=document.getElementById("service-name")
+let callButton=document.getElementById("first-button")
+let serviceName=document.getElementsByClassName("service-name")
 let number=document.getElementById("Number")
 let coin=document.getElementById("coin")
+let divHistory=document.getElementById("div-history")
+let clearButton=document.getElementById("btn-clear")
+let parentHistory=document.getElementById("parent-history")
+let calls=document.getElementsByClassName("call")
 let count=0
 
 heart.addEventListener("click",function(){
@@ -14,9 +18,25 @@ heart.addEventListener("click",function(){
 })
 
 
-callButton.addEventListener("click", function(){
-    let servic=serviceName.innerText
-    let callNumber=number.innerText
+
+clearButton.addEventListener("click",function(){
+    parentHistory.removeChild(divHistory)
+
+})
+
+for (const call of calls){
+    call.addEventListener("click",function(){
+
+    let card=call.closest(".card")
+
+    let servic = card.querySelector(".service-name").innerText;
+    let callNumber = card.querySelector(".number").innerText;
+
+
+
+
+    
+  
       
     let num=parseInt(coin.innerText)
      if(num<20){
@@ -30,9 +50,19 @@ callButton.addEventListener("click", function(){
   
    
     alert( `📞 "calling" ${servic} ${callNumber}  `)
-  
+    let date=new Date()
+    let currentDate=date.toLocaleTimeString()
 
+   let child= document.createElement("div")
+   child.innerHTML=`  <div id="div-history" class="bg-[#FAFAFA] flex justify-between items-center p-2 mt-4 rounded-lg">
+        <div  class="w-[60%]">
+                <h1 id="sirvice-history" class="font-semibold">${servic}</h1>
+                <p id="call-history">${callNumber}</p>
 
-   
-    
-})
+        </div>
+        <div> <h1 id="time">${currentDate}</h1></div>
+
+    </div>`
+    parentHistory.appendChild(child)
+    })
+}
