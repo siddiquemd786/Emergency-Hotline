14bbeaf231f805b6779693ae28f98a8ad526ff98
siddique <siddiquemd161@gmail.com>
2025-08-28 13:12:44 +0600

    history section is dynamic

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let heartNav=document.getElementById("nav-heart")
 let heart=document.getElementById("card-heart")
 let callButton=document.getElementById("first-button")
 let serviceName=document.getElementsByClassName("service-name")
-let number=document.getElementById("Number")
+let number=document.getElementsByClassName("Number")
 let coin=document.getElementById("coin")
 let divHistory=document.getElementById("div-history")
 let clearButton=document.getElementById("btn-clear")
@@ -17,46 +17,41 @@ heart.addEventListener("click",function(){
 
 })
 
+console.log(number)
+
 
 
 clearButton.addEventListener("click",function(){
-    parentHistory.removeChild(divHistory)
+
+  divHistory.hidden = true; 
+  parentHistory.hidden = false;
+  
+
 
 })
 
-for (const call of calls){
-    call.addEventListener("click",function(){
-
-    let card=call.closest(".card")
-
-    let servic = card.querySelector(".service-name").innerText;
-    let callNumber = card.querySelector(".number").innerText;
 
 
 
+   function callHandaler (index){ 
+   let service= serviceName[index].innerText
+     let callNumber=number[index].innerText   
 
-    
-  
-      
+          alert( `📞 "calling" ${service} ${callNumber}  `)
     let num=parseInt(coin.innerText)
      if(num<20){
     alert("Not enough coins!");
         return;
    }
-    
-
    let result=num-20
    coin.innerText=result
-  
-   
-    alert( `📞 "calling" ${servic} ${callNumber}  `)
     let date=new Date()
     let currentDate=date.toLocaleTimeString()
 
    let child= document.createElement("div")
    child.innerHTML=`  <div id="div-history" class="bg-[#FAFAFA] flex justify-between items-center p-2 mt-4 rounded-lg">
         <div  class="w-[60%]">
-                <h1 id="sirvice-history" class="font-semibold">${servic}</h1>
+                <h1 id="sirvice-history" class="font-semibold">${service}</h1>
                 <p id="call-history">${callNumber}</p>
 
         </div>
@@ -64,5 +59,12 @@ for (const call of calls){
 
     </div>`
     parentHistory.appendChild(child)
-    })
+    }
+
+
+
+ for (let i = 0; i < calls.length; i++) {
+  calls[i].addEventListener("click", function() {
+    callHandaler(i);
+  });
 }
